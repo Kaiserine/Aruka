3d66ff992155444943bd115e8c5f1577473827b3
ナニカ <46721874+Kaiserine@users.noreply.github.com>
2019-01-18 13:01:05 +0100

    Update chuck.js

--- commandes/chuck.js
+++ commandes/chuck.js
@@ -1,21 +1,39 @@
-function reverseHtmleEntities(texte) {
-    texte = texte.replace(/&egrave;/g, 'è');
-    texte = texte.replace(/&eacute;/g, 'é');
-    texte = texte.replace(/&ecirc;/g, 'ê');
-    texte = texte.replace(/&euml;/g, 'ë');
-    texte = texte.replace(/&agrave;/g, 'à');
-    texte = texte.replace(/&acirc;/g, 'ä');
-    texte = texte.replace(/&auml;/g, 'â');
-    texte = texte.replace(/&ograve;/g, 'ò');
-    texte = texte.replace(/&ocirc;/g, 'ô');
-    texte = texte.replace(/&ouml;/g, 'ö');
-    texte = texte.replace(/&igrave;/g, 'ì');
-    texte = texte.replace(/&icirc;/g, 'î');
-    texte = texte.replace(/&iuml;/g, 'ï');
-    texte = texte.replace(/&ugrave;/g, 'ù');
-    texte = texte.replace(/&uuml;/g, 'ü');
-    texte = texte.replace(/&ucirc;/g, 'û');
-    return texte ;
+const entiteHTMLlist= [
+	'&nbsp;': 160,		'&iexcl;': 161,		'&cent;': 162,		'&pound;': 163,		'&curren;': 164,
+	'&yen;': 165,		'&brvbar;': 166,	'&sect;': 167,		'&uml;': 168,		'&copy;': 169,
+	'&ordf;': 170,		'&laquo;': 171,		'&not;': 172,		'&shy;': 173,		'&reg;': 174,
+	'&macr;': 175,		'&deg;': 176,		'&plusmn;': 177,	'&sup2;': 178,		'&sup3;': 179,
+	'&acute;': 180,		'&micro;': 181,		'&para;': 182,		'&middot;': 183,	'&cedil;': 184,
+	'&sup1;': 185,		'&ordm;': 186,		'&raquo;': 187,		'&frac14;': 188,	'&frac12;': 189,
+	'&frac34;': 190,	'&iquest;': 191,	'&Agrave;': 192,	'&Aacute;': 193,	'&Acirc;': 194,
+	'&Atilde;': 195,	'&Auml;': 196,		'&Aring;': 197,		'&AElig;': 198,		'&Ccedil;': 199,
+	'&Egrave;': 200,	'&Eacute;': 201,	'&Ecirc;': 202,		'&Euml;': 203,		'&Igrave;': 204,
+	'&Iacute;': 205,	'&Icirc;': 206,		'&Iuml;': 207,		'&ETH;': 208,		'&Ntilde;': 209,
+	'&Ograve;': 210,	'&Oacute;': 211,	'&Ocirc;': 212,		'&Otilde;': 213,	'&Ouml;': 214,
+	'&times;': 215,		'&Oslash;': 216,	'&Ugrave;': 217,	'&Uacute;': 218,	'&Ucirc;': 219,
+	'&Uuml;': 220,		'&Yacute;': 221,	'&THORN;': 222,		'&szlig;': 223,		'&agrave;': 224,
+	'&aacute;': 225,	'&acirc;': 226,		'&atilde;': 227,	'&auml;': 228,		'&aring;': 229,
+	'&aelig;': 230,		'&ccedil;': 231,	'&egrave;': 232,	'&eacute;': 233,	'&ecirc;': 234,
+	'&euml;': 235,		'&igrave;': 236,	'&iacute;': 237,	'&icirc;': 238,		'&iuml;': 239,
+	'&eth;': 240,		'&ntilde;': 241,	'&ograve;': 242,	'&oacute;': 243,	'&ocirc;': 244,
+	'&otilde;': 245,	'&ouml;': 246,		'&divide;': 247,	'&oslash;': 248,	'&ugrave;': 249,
+	'&uacute;': 250,	'&ucirc;': 251,		'&uuml;': 252,		'&yacute;': 253,	'&thorn;': 254,
+	'&yuml;': 255,		'&amp;': 38
+];
+
+const entiteHTML= (texte)=> {
+	return /&[a-zA-Z0-9]+;/gi[Symbol.replace](texte, (a)=> {
+		let hex= /&#([a-fA-F0-9]+);/;
+		if(hex.test(a)) {
+			return hex[Symbol.replace](a, (a,b)=> {
+				return String.fromCharCode(parseInt(b, 10));
+			});
+		} else if(entiteHTMLlist.hasOwnProperty(a)) {
+			return String.fromCharCode(entiteHTMLlist[a]);
+		} else {
+			return '?';
+		}
+	});
 };
 
 const request = require('request');
@@ -28,9 +46,7 @@ const run= async(robot, message, commandes, prefixe)=> {
 			let fact= JSON.parse(corps)[0];
 			message.channel.send({embed: {
 				title: "Chuck Norris Facts",
-				description: reverseHtmleEntities(/&#([a-fA-F0-9]+);/gi[Symbol.replace](fact.fact, (a,b)=> {
-					return String.fromCharCode(parseInt(b, 10));
-				})),
+				description: entiteHTML(fact.fact),
 				color: 15143599,
 				footer: {text: `id:${fact.id} | vote:${fact.vote} | points:${fact.points}`}
 			}});
