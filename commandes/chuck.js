30a0c39e84cfe4bc6d5ea85eca62f24fc787e7a5
ナニカ <46721874+Kaiserine@users.noreply.github.com>
2019-01-18 09:55:08 +0100

    Update chuck.js

--- commandes/chuck.js
+++ commandes/chuck.js
@@ -1,16 +1,19 @@
-/*
-http://chucknorrisfacts.fr/api/get?data=tri:alea;nb:1
-{
-  embed: {
-    title: "Chuck Norris Facts",
-    description: fact.fact,
-    color: 15143599,
-    footer: {text: `id:${fact.id} | vote:${fact.vote} | points:${fact.points}`}
-  }
-}
-*/
+const request = require('request');
 const run= async(robot, message, commandes, prefixe)=> {
-	message.channel.send('En cours de dévellopements...');
+	request('http://chucknorrisfacts.fr/api/get?data=tri:alea;nb:1', (erreur, response, corps)=> {
+		if(erreur) {
+			console.log(erreur);
+			message.channel.send('Hu hu hu, une erreur est survenu ^^');
+		} else {
+			let fact= JSON.parse(corps);
+			message.channel.send({embed: {
+				title: "Chuck Norris Facts",
+				description: fact.fact,
+				color: 15143599,
+				footer: {text: `id:${fact.id} | vote:${fact.vote} | points:${fact.points}`}
+			}});
+		}
+	});
 };
 const name= 'chuck';
 const alias= ['c', 'noris'];
