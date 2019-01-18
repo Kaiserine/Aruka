067af8c62454d617a31a486ff14137d40b40a05c
ナニカ <46721874+Kaiserine@users.noreply.github.com>
2019-01-18 10:49:32 +0100

    Update chuck.js

--- commandes/chuck.js
+++ commandes/chuck.js
@@ -1,3 +1,23 @@
+function reverseHtmleEntities(texte) {
+    texte = texte.replace(/&egrave;/g, 'è');
+    texte = texte.replace(/&eacute;/g, 'é');
+    texte = texte.replace(/&ecirc;/g, 'ê');
+    texte = texte.replace(/&euml;/g, 'ë');
+    texte = texte.replace(/&agrave;/g, 'à');
+    texte = texte.replace(/&acirc;/g, 'ä');
+    texte = texte.replace(/&auml;/g, 'â');
+    texte = texte.replace(/&ograve;/g, 'ò');
+    texte = texte.replace(/&ocirc;/g, 'ô');
+    texte = texte.replace(/&ouml;/g, 'ö');
+    texte = texte.replace(/&igrave;/g, 'ì');
+    texte = texte.replace(/&icirc;/g, 'î');
+    texte = texte.replace(/&iuml;/g, 'ï');
+    texte = texte.replace(/&ugrave;/g, 'ù');
+    texte = texte.replace(/&uuml;/g, 'ü');
+    texte = texte.replace(/&ucirc;/g, 'û');
+    return texte ;
+};
+
 const request = require('request');
 const run= async(robot, message, commandes, prefixe)=> {
 	request('http://chucknorrisfacts.fr/api/get?data=tri:alea;nb:1', (erreur, response, corps)=> {
@@ -8,9 +28,9 @@ const run= async(robot, message, commandes, prefixe)=> {
 			let fact= JSON.parse(corps)[0];
 			message.channel.send({embed: {
 				title: "Chuck Norris Facts",
-				description: /&#([a-fA-F0-9]+);/gi[Symbol.replace](fact.fact, (a,b)=> {
+				description: reverseHtmleEntities(/&#([a-fA-F0-9]+);/gi[Symbol.replace](fact.fact, (a,b)=> {
 					return String.fromCharCode(parseInt(b, 10));
-				}),
+				})),
 				color: 15143599,
 				footer: {text: `id:${fact.id} | vote:${fact.vote} | points:${fact.points}`}
 			}});
