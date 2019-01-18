2d2cb14244ac5b363af2b913836384ac2e05dc1f
ナニカ <46721874+Kaiserine@users.noreply.github.com>
2019-01-18 09:16:48 +0100

    Update pipotron.js

--- commandes/pipotron.js
+++ commandes/pipotron.js
@@ -7,3 +7,11 @@ let c= [];
 	c.join(' ');
 
 */
+const run= async(robot, message, commandes, prefixe)=> {
+	message.channel.send('En cours de dévellopements...');
+};
+const name= 'pipotron';
+const alias= ['pipo'];
+const description= 'Génére une phrase aléatoire!';
+const aide= 'hum..';
+module.exports= {run, name, alias, description, aide};
